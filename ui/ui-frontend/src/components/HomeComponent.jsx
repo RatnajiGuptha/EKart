c4b5aa9cc34c6c6f913e5d07275ed4c1a79f145e
Harsha Sri Bandi <harsha_bandi@persistent.com>
2023-05-04 10:56:23 +0530

    modified caurosel

--- ui/ui-frontend/src/components/HomeComponent.jsx
+++ ui/ui-frontend/src/components/HomeComponent.jsx
@@ -1,48 +1,110 @@
 import React, { Component } from 'react';
 import '../StyleSheets/Home.css'
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../images/caurosel-img1.png';
+import img2 from '../images/caurosel-img2.png';
+import img3 from '../images/caurosel-img3.png';
+import img4 from '../images/caurosel-img4.png';
+import img5 from '../images/caurosel-img5.png';
+import img6 from '../images/kids wear.png';
+import img7 from '../images/beauty.png';
 
 class HomeComponent extends Component {
     render() {
         return (
-            <div className='fluid-container'>  
-               <div className='d-flex '>
-                <div className='card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100" className='w-75'/>
-                    <p className='card-names'>   Applications</p>
-                </div>
-                <div className='cd-3 card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/0d75b34f7d8fbcb3.png?q=100" className='w-75'/>
-                    <p className='card-names'>   Beauty </p>
-                </div>
-                <div className='cd-3 card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100" className='w-75'/>
-                    <p className='card-names'>  Electronics </p>
-                </div>
-                <div className='cd-3 card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/0d75b34f7d8fbcb3.png?q=100" className='w-75'/>
-                    <p className='card-names'>   Fashion </p>
-                </div>
-                <div className='cd-3 card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100" className='w-75'/>
-                    <p className='card-names'>   Grocery </p>
-                </div>
-                <div className='cd-3 card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100" className='w-75 p-1'/>
-                    <p className='card-names'>   Home </p>
-                </div>
-                <div className='cd-3 card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" className='w-75'/>
-                    <p className='card-names'>   Mobiles </p>
-                </div>
-                <div className='cd-3 card-info m-2'>
-                    <img src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100" className='w-75'/>
-                    <p className='card-names'>   Toys </p>
-                </div>
-                
-                
-               
+            <div className='fluid-container'>
+                <div className='d-flex flex-row justify-content-center'>
+                    <div className='card-info'>
+                        <img src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100" className='w-75' />
+                        <p className='card-names'>  Accessories</p>
+                    </div>
+                    <div className='card-info'>
+                        <img src={img7} className='w-75' />
+                        <p className='card-names'>  Beauty </p>
+                    </div>
+                    <div className='card-info'>
+                        <img src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100" className='w-75' />
+                        <p className='card-names'>   Electronics </p>
+                    </div>
+                    <div className='card-info'>
+                        <img src="https://rukminim1.flixcart.com/fk-p-flap/128/128/image/0d75b34f7d8fbcb3.png?q=100" className='w-75' />
+                        <p className='card-names'>   Fashion </p>
+                    </div>
+                    <div className='card-info'>
+                        <img src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100" className='w-75' />
+                        <p className='card-names'>   Grocery </p>
+                    </div>
+                    <div className='card-info'>
+                        <img src="https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100" className='w-75 p-1' />
+                        <p className='card-names'>   Appliances</p>
+                    </div>
+                    <div className='card-info'>
+                        <img src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" className='w-75' />
+                        <p className='card-names'>    Mobiles </p>
+                    </div>
+                    <div className='card-info'>
+                        <img src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100" className='w-75' />
+                        <p className='card-names'>   Toys </p>
+                    </div>
 
-               </div>
+                    <div className='card-info'>
+                        <img src={img6} className='w-75'/>
+                        <p className='card-names'> Kids wear</p>
+                    </div>
+
+                </div>
+                <div >
+                    <Carousel className="carousalClass">
+                        <Carousel.Item >
+                            <img
+                                className="d-block w-100 carousalImgClass "
+                                src={img1}
+                                alt="First slide"
+                            />
+                            {/* <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption> */}
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carousalImgClass"
+                                src={img2}
+                                alt="Second slide"
+                            />
+                
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carousalImgClass"
+                                src={img3}
+                                alt="Third slide"
+                            />
+                            {/* <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>
+                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                                </p>
+                            </Carousel.Caption> */}
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carousalImgClass"
+                                src={img4}
+                                alt="Second slide"
+                            />
+                
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carousalImgClass"
+                                src={img5}
+                                alt="Second slide"
+                            />
+                
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
             </div>
         );
     }
