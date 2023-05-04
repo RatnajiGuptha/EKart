e7bcf714079c52ba120d511f47707d8f15a64a97
shyam kumar <shyamkumarhasti@gmail.com>
2023-05-04 10:44:26 +0530

    initial backend project

---
+++ backend/eKart-backend/src/test/java/com/eKart/eKartbackend/EKartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eKart.eKartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EKartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
