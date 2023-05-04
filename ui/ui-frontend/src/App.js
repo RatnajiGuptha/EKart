d421e37e471b804b34ccfde7f62e58dd3253415a
guptha <ratnajiguptha819@gmail.com>
2023-05-04 14:20:50 +0530

    login page added

--- ui/ui-frontend/src/App.js
+++ ui/ui-frontend/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeaderComponent from "./components/HeaderComponent.jsx"
 import HomeComponent from './components/HomeComponent';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <HeaderComponent />
-        <HomeComponent/>
+          {/* <HomeComponent/> */}
+        <LoginPage/>
       </Router >
     </div >
   );
--- ui/ui-frontend/src/components/HeaderComponent.jsx
+++ ui/ui-frontend/src/components/HeaderComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -6,43 +6,43 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 
 
-class headerComponent extends Component {
-    render() {
-        return (
-            <Navbar className='navbar navbar-dark bg-dark'>
-                <Navbar fixed="top" />
-                <Container>
-                    <Navbar.Brand href="/" className='m-auto'>E-Kart</Navbar.Brand>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Container>
-                            <Form className="d-flex">
-                                <Form.Control type="search" placeholder="Search" className='m-auto text-dark w-75 ' aria-label='Search' />
-                            </Form>
-                        </Container>
-                        <Container>
-                            <Nav className='m-auto'>
-                                <Nav.Link href='/' > Home</Nav.Link>
-                                <NavDropdown title="Categories" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href='/'>Appliances</NavDropdown.Item>
-                                    <NavDropdown.Item href='/'>Beauty</NavDropdown.Item>
-                                    <NavDropdown.Item href="/">Electronics</NavDropdown.Item>
-                                    <NavDropdown.Item href="/">Fashion</NavDropdown.Item>
-                                    <NavDropdown.Item href='/'>Grocery</NavDropdown.Item>
-                                    <NavDropdown.Item href="/">Home</NavDropdown.Item>
-                                    <NavDropdown.Item href='/'>Mobiles</NavDropdown.Item>
-                                    <NavDropdown.Item href='/'>Toys</NavDropdown.Item>
-                                </NavDropdown>
-                                <Nav.Link href="/">Help</Nav.Link>
-                                <Nav.Link href="/" >Login</Nav.Link>
-                                <Nav.Link href="/" >Become a Seller</Nav.Link>
-                            </Nav>
-                        </Container>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar >
-        );
-    }
+function headerComponent() {
+
+    return (
+        <Navbar className='navbar navbar-dark bg-dark'>
+            <Navbar fixed="top" />
+            <Container>
+                <Navbar.Brand href="/" className='m-auto'>E-Kart</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    {/* <Container>
+                        <Form className="d-flex">
+                            <Form.Control type="search" placeholder="Search" className='m-auto text-dark w-75 ' aria-label='Search' />
+                        </Form>
+                    </Container> */}
+                    <Container>
+                        <Nav className='m-auto'>
+                            <Nav.Link href='/' > Home</Nav.Link>
+                            <NavDropdown title="Categories" id="basic-nav-dropdown">
+                                <NavDropdown.Item href='/'>Appliances</NavDropdown.Item>
+                                <NavDropdown.Item href='/'>Beauty</NavDropdown.Item>
+                                <NavDropdown.Item href="/">Electronics</NavDropdown.Item>
+                                <NavDropdown.Item href="/">Fashion</NavDropdown.Item>
+                                <NavDropdown.Item href='/'>Grocery</NavDropdown.Item>
+                                <NavDropdown.Item href="/">Home</NavDropdown.Item>
+                                <NavDropdown.Item href='/'>Mobiles</NavDropdown.Item>
+                                <NavDropdown.Item href='/'>Toys</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="/">Help</Nav.Link>
+                            <Nav.Link href="/" >Login</Nav.Link>
+                            <Nav.Link href="/" >Become a Seller</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
+    );
+
 }
 
 export default headerComponent;
