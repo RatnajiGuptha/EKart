62083c8a750083b68a07d728ad25631109d88f24
Pothu Raju Neduri <pothu_neduri@persistent.com>
2023-05-05 11:31:46 +0530

    Item details added

--- ui/ui-frontend/src/App.js
+++ ui/ui-frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeaderComponent from "./components/HeaderComponent.jsx"
 import HomeComponent from './components/HomeComponent';
+import ItemDetails from './components/ItemDetails';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <HeaderComponent />
         <HomeComponent/>
+        {/* <ItemDetails/> */}
       </Router >
     </div >
   );
