871a9dbdf14d8852a5e3d11fb155487d7c6c7cf9
Pothu Raju Neduri <pothu_neduri@persistent.com>
2023-05-09 10:40:28 +0530

    product page changes done

--- ui/ui-frontend/src/components/Products/Products.jsx
+++ ui/ui-frontend/src/components/Products/Products.jsx
@@ -1,37 +1,11 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import "./style.css";
-const Products = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((response) => {
-      console.log(response);
-      setProducts(response.data);
-    }, []);
-  });
+import Product from "../Product/Product";
 
+const Products = ({ products }) => {
+  console.log("Products ===>", products);
   return (
     <div>
-      {products.map((item) => {
-        return (
-          <div>
-            <div>
-              <img className="image_link" src={item.image} alt="/"></img>
-              <div className="sub1-card">
-                <div>
-                  <p>{item.title}</p>
-                </div>
-                <div>
-                  <p>{item.price}</p>
-                </div>
-                {/* <p>{item.description}</p> */}
-                <div>
-                  <p>{item.category}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
+      {products.map((product) => {
+        return <Product product={product} />;
       })}
     </div>
   );
