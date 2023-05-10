4f3fa24152ac4651364816d16ac71d5877d19223
Harsha Sri Bandi <harsha_bandi@persistent.com>
2023-05-10 16:44:53 +0530

    backend changes commited

--- ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/FashionProductsController.java
+++ ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/FashionProductsController.java
@@ -3,23 +3,29 @@ package com.ekart.inventory.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ekart.inventory.entity.FashionProducts;
 import com.ekart.inventory.enums.FashionTypes;
 import com.ekart.inventory.service.FashionProductService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/fashionProducts")
 public class FashionProductsController {
 
 	@Autowired
 	private FashionProductService fashionService;
+
+
+	@PostMapping("/addMultipleProducts")
+	public String addMultipleProduct(@RequestBody List<FashionProducts> fashionProducts) {
+
+		for(FashionProducts  prods : fashionProducts) {
+			String response = fashionService.saveFashionProduct(prods);
+		}
+		return "Multiple Items added";
+	}
 	
 	@PostMapping("/add")
 	public String addFashionProduct(@RequestBody FashionProducts fashionProducts) {
