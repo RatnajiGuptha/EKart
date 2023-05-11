971a64452771a1e23ceeee8cde82d50bd375997e
guptha <ratnajiguptha819@gmail.com>
2023-05-11 11:56:24 +0530

    backend

--- ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/FashionProductsController.java
+++ ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/FashionProductsController.java
@@ -9,7 +9,7 @@ import com.ekart.inventory.entity.FashionProducts;
 import com.ekart.inventory.enums.FashionTypes;
 import com.ekart.inventory.service.FashionProductService;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/fashionProducts")
 public class FashionProductsController {
@@ -17,32 +17,32 @@ public class FashionProductsController {
 	@Autowired
 	private FashionProductService fashionService;
 
-
 	@PostMapping("/addMultipleProducts")
 	public String addMultipleProduct(@RequestBody List<FashionProducts> fashionProducts) {
 
-		for(FashionProducts  prods : fashionProducts) {
+		for (FashionProducts prods : fashionProducts) {
 			String response = fashionService.saveFashionProduct(prods);
 		}
 		return "Multiple Items added";
 	}
-	
+
 	@PostMapping("/add")
 	public String addFashionProduct(@RequestBody FashionProducts fashionProducts) {
-		String response =fashionService.saveFashionProduct(fashionProducts);
+		String response = fashionService.saveFashionProduct(fashionProducts);
 		return response;
 	}
-	
+
 	@GetMapping("/getProducts")
-	public List<FashionProducts> getFashionProducts(){
+	public List<FashionProducts> getFashionProducts() {
 		List<FashionProducts> fashionProduct = fashionService.loadFashionProducts();
 		return fashionProduct;
 	}
-	
+
 	@GetMapping("/getProducts/type/{type}")
-	public List<FashionProducts> getFashionProductsByType(@PathVariable FashionTypes type){
+	public List<FashionProducts> getFashionProductsByType(@PathVariable FashionTypes type) {
 		List<FashionProducts> fetchByTypes = fashionService.fetchByType(type);
 		return fetchByTypes;
 	}
+
 	
 }
