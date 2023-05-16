f89fb1c8fe35ddf932d127d4116e3d4b342abb1a
guptha <ratnajiguptha819@gmail.com>
2023-05-16 16:27:09 +0530

    backend minor changes done

--- ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/BeautyController.java
+++ ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/BeautyController.java
@@ -1,11 +1,11 @@
 package com.ekart.inventory.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,41 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ekart.inventory.entity.Beauty;
 import com.ekart.inventory.service.BeautyService;
 
-
 @RestController
 @RequestMapping("/api/beauty")
+@CrossOrigin("http://localhost:3000/")
 public class BeautyController {
 
-    @Autowired
-    private BeautyService beautyService;
+	@Autowired
+	private BeautyService beautyService;
 
-    @PostMapping("/addMultipleBeautyProducts")
-    public ResponseEntity<String> saveAllBeautyProducts(@RequestBody List<Beauty> beautyProductsList){
+	@PostMapping("/addMultipleBeautyProducts")
+	public ResponseEntity<String> saveAllBeautyProducts(@RequestBody List<Beauty> beautyProductsList) {
 
-        for(Beauty beauty : beautyProductsList){
-            String response = beautyService.addBeautyProducts(beauty);
-        }
-        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("Added multiple beauty products");
-    }
+		for (Beauty beauty : beautyProductsList) {
+			String response = beautyService.addBeautyProducts(beauty);
+		}
+		return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("Added multiple beauty products");
+	}
 
-    @PostMapping("/add")
-    public ResponseEntity<String> saveBeautyProducts(@RequestBody Beauty beauty){
-            String response = beautyService.addBeautyProducts(beauty);
-        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(response);
-    }
+	@PostMapping("/add")
+	public ResponseEntity<String> saveBeautyProducts(@RequestBody Beauty beauty) {
+		String response = beautyService.addBeautyProducts(beauty);
+		return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(response);
+	}
 
-    @GetMapping("/getBeautyProducts")
-    public ResponseEntity<List<Beauty>> fetchAllBeautyProducts(){
-        List<Beauty> beautyProductsList = beautyService.getAllBeautyProducts();
+	@GetMapping("/getBeautyProducts")
+	public ResponseEntity<List<Beauty>> fetchAllBeautyProducts() {
+		List<Beauty> beautyProductsList = beautyService.getAllBeautyProducts();
 
-        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(beautyProductsList);
-    }
+		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(beautyProductsList);
+	}
 
-    @GetMapping("/getBeautyById/{id}")
-    public ResponseEntity<Beauty> fetchByBeautyProductId(@PathVariable int id){
-        Beauty beauty = beautyService.getBeautyById(id);
+	@GetMapping("/getBeautyById/{id}")
+	public ResponseEntity<Beauty> fetchByBeautyProductId(@PathVariable int id) {
+		Beauty beauty = beautyService.getBeautyById(id);
 
-        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(beauty);
-    }
+		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(beauty);
+	}
 
 }
--- ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/FashionProductsController.java
+++ ekart-backend/Inventory-service/src/main/java/com/ekart/inventory/controller/FashionProductsController.java
@@ -18,60 +18,67 @@ import com.ekart.inventory.enums.FashionTypes;
 import com.ekart.inventory.enums.Suitable;
 import com.ekart.inventory.service.FashionProductService;
 
-@CrossOrigin("http://localhost:3000/")
 @RestController
 @RequestMapping("/api/fashionProducts")
+@CrossOrigin("http://localhost:3000/")
 public class FashionProductsController {
 
 	@Autowired
 	private FashionProductService fashionService;
 
-
 	@PostMapping("/addMultipleProducts")
 	public ResponseEntity<String> addMultipleProduct(@RequestBody List<FashionProducts> fashionProducts) {
 
-		for(FashionProducts  prods : fashionProducts) {
+		for (FashionProducts prods : fashionProducts) {
 			fashionService.saveFashionProduct(prods);
 		}
 		return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("Multiple fashion products added");
 	}
-	
+
 	@PostMapping("/add")
 	public ResponseEntity<String> addFashionProduct(@RequestBody FashionProducts fashionProducts) {
 		fashionService.saveFashionProduct(fashionProducts);
 		return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("Fashion product added");
 	}
-	
+
 	@GetMapping("/getProducts")
-	public ResponseEntity<List<FashionProducts>> getFashionProducts(){
+	public ResponseEntity<List<FashionProducts>> getFashionProducts() {
 		List<FashionProducts> fashionProduct = fashionService.loadFashionProducts();
 		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(fashionProduct);
 	}
-	
+
 	@GetMapping("/getProducts/type/{type}")
-	public ResponseEntity<List<FashionProducts>> getFashionProductsByType(@PathVariable FashionTypes type){
+	public ResponseEntity<List<FashionProducts>> getFashionProductsByType(@PathVariable FashionTypes type) {
 		List<FashionProducts> fetchByTypes = fashionService.fetchByType(type);
 		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(fetchByTypes);
 	}
-	
+
 	@GetMapping("/getProducts/type/{type}/{productId}")
-	public ResponseEntity<FashionProducts> getFashionProductsByTypeAndId(@PathVariable FashionTypes type,@PathVariable int productId ){
+	public ResponseEntity<FashionProducts> getFashionProductsByTypeAndId(@PathVariable FashionTypes type,
+			@PathVariable int productId) {
 //		List<FashionProducts> fetchByTypes = fashionService.fetchByType(type);
 		FashionProducts product = fashionService.fetchById(productId);
 		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(product);
 	}
-	
+
 	@GetMapping("/getProducts/{productId}")
 	public ResponseEntity<FashionProducts> getFashionProductsById(@PathVariable int productId) {
 		FashionProducts product = fashionService.fetchById(productId);
 		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(product);
 	}
-	
+
 	@GetMapping("/getProducts/suitableFor/{suitable}")
-	public ResponseEntity<List<FashionProducts>> getFashionProductsBySuitable(@PathVariable Suitable suitable){
+	public ResponseEntity<List<FashionProducts>> getFashionProductsBySuitable(@PathVariable Suitable suitable) {
 		List<FashionProducts> fashionProducts = fashionService.GetFashionProductsBySuitable(suitable);
 		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(fashionProducts);
 	}
-	
-	
+
+	@GetMapping("/getProducts/suitableFor/{suitable}/{productId}")
+	public ResponseEntity<FashionProducts> getFashionProductsBySuitableById(@PathVariable Suitable suitable,
+			@PathVariable int productId) {
+		List<FashionProducts> fashionProducts = fashionService.GetFashionProductsBySuitable(suitable);
+		FashionProducts product = fashionService.fetchById(productId);
+		return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(product);
+	}
+
 }
