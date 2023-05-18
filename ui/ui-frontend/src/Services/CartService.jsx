9ada652915b570c12c6b953fc8f7845e3b151991
raju neduri <rajuneduri2255@gmail.com>
2023-05-18 15:56:21 +0530

    deleted cart product items

--- ui/ui-frontend/src/Services/CartService.jsx
+++ ui/ui-frontend/src/Services/CartService.jsx
@@ -3,16 +3,16 @@ import axios from "axios";
 const CART_SERVICE_BASE_URL = "http://localhost:8300/api/cart";
 
 class CartService {
+  addItemsToCart(cart) {
+    return axios.post(CART_SERVICE_BASE_URL + "/add", cart);
+  }
 
-    addItemsToCart(cart) {
-        return axios.post(CART_SERVICE_BASE_URL + "/add", cart);
-    }
-
-    getAllItemsInCart() {
-        return axios.get(CART_SERVICE_BASE_URL + "/getProducts")
-    }
-
+  getAllItemsInCart() {
+    return axios.get(CART_SERVICE_BASE_URL + "/getProducts");
+  }
+  deleteItemFromCart(cartId) {
+    axios.delete(CART_SERVICE_BASE_URL + "/deleteProductInCart/" + cartId);
+  }
 }
-
 
 export default new CartService();
--- ui/ui-frontend/src/components/CartComponent.jsx
+++ ui/ui-frontend/src/components/CartComponent.jsx
@@ -5,72 +5,101 @@ import CartService from "../Services/CartService";
 import "../StyleSheets/Cart.css";
 
 const CartComponent = () => {
+  const [cartItems, setCartItems] = useState([]);
 
-    const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    CartService.getAllItemsInCart().then((response) => {
+      setCartItems(response.data);
+      console.log(response.data);
+    });
+  }, []);
 
-    useEffect(() => {
-        CartService.getAllItemsInCart().then((response) => {
-            setCartItems(response.data);
-            console.log(response.data);
-        });
-    }, []);
+  const deleteItemFromCarttt = async (cartId) => {
+    // console.log(cartId);
+    CartService.deleteItemFromCart(cartId);
+    alert("Item Deleted Successfully");
+    window.location.reload(false);
+  };
 
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    let count = 0;
 
-    const calculateTotalPrice = () => {
-        let totalPrice = 0;
-        let count = 0
-
-        for (const item of cartItems) {
-            totalPrice += item.productPrice * item.qty;
-            count += 1
-        }
-
-        return { totalPrice, count };
+    for (const item of cartItems) {
+      totalPrice += item.productPrice * item.qty;
+      count += 1;
     }
 
-    return (
-        <div className="d-flex justify-content-center">
-            <div className="cart-container">
-                {cartItems.map((item) => (
-                    <div key={item.id} className="items-container" >
-                        <div>
-                            <img src={item.logoImg} className="cart-product-img" alt="/"></img>
-                        </div>
-                        <div className="cart-item-details">
-                            <h5 className="product-name">{item.productName}</h5>
-                            <p className="product-description">  {item.productDescription}</p>
-                            <div className="d-flex">
-                                <div className={`size-selector`}>
-                                    <span className='size-text'> Color:{item.color} </span>
-                                </div>
-                                <div className={`size-selector`}>
-                                    <span className='size-text'> Size:{item.size} </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <p className="product-price"> Quantity: <span style={{ fontStyle: "italic", fontWeight: "bold" }}>{item.qty} </span></p>
-                            <p className="product-price">Price: ₹ <span style={{ fontStyle: "italic", fontWeight: "bold" }}>{item.productPrice}/-</span></p>
-                        </div>
-                        <div className="m-2">
-                            <button className="btn btn-danger">Remove From Cart</button>
-                        </div>
-                    </div>
-                ))}
+    return { totalPrice, count };
+  };
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="cart-container">
+        {cartItems.map((item) => (
+          <div key={item.id} className="items-container">
+            <div>
+              <img
+                src={item.logoImg}
+                className="cart-product-img"
+                alt="/"
+              ></img>
+            </div>
+            <div className="cart-item-details">
+              <h5 className="product-name">{item.productName}</h5>
+              <p className="product-description"> {item.productDescription}</p>
+              <div className="d-flex">
+                <div className={`size-selector`}>
+                  <span className="size-text"> Color:{item.color} </span>
+                </div>
+                <div className={`size-selector`}>
+                  <span className="size-text"> Size:{item.size} </span>
+                </div>
+              </div>
             </div>
             <div>
-                <div className="price-container table" >
-                    <h3> Price Details</h3>
-                    <p> Total Products: {calculateTotalPrice().count}</p>
-                    <p className="total-price">  Total Price:<span style={{ fontStyle: "italic" }}> ₹ {calculateTotalPrice().totalPrice}/- </span></p>
-                    <button className="checkout-btn">Checkout</button>
-                </div>
+              <p className="product-price">
+                {" "}
+                Quantity:{" "}
+                <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                  {item.qty}{" "}
+                </span>
+              </p>
+              <p className="product-price">
+                Price: ₹{" "}
+                <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                  {item.productPrice}/-
+                </span>
+              </p>
             </div>
+            <div className="m-2">
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteItemFromCarttt(item.cartId)}
+              >
+                Remove From Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <div className="price-container table">
+          <h3> Price Details</h3>
+          <p> Total Products: {calculateTotalPrice().count}</p>
+          <p className="total-price">
+            {" "}
+            Total Price:
+            <span style={{ fontStyle: "italic" }}>
+              {" "}
+              ₹ {calculateTotalPrice().totalPrice}/-{" "}
+            </span>
+          </p>
+          <button className="checkout-btn">Checkout</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
-
-
-
 
 export default CartComponent;
