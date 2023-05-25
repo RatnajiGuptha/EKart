61e9ef1c77ba77043c2c7952791c4cb46eadbc75
guptha <ratnajiguptha819@gmail.com>
2023-05-25 12:32:07 +0530

    implemented changes in backend

--- ekart-backend/Gateway-service/src/main/java/com/ekart/gateway/service/AddressService.java
+++ ekart-backend/Gateway-service/src/main/java/com/ekart/gateway/service/AddressService.java
@@ -2,15 +2,17 @@ package com.ekart.gateway.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.ekart.gateway.entity.Address;
 
+@Service
 public interface AddressService {
 
-	
 	public String addNewAddress(Address address);
-	
+
 	public List<Address> getAllAddress();
-	
+
 	public Address fetchById(int id);
-	
+
 }
