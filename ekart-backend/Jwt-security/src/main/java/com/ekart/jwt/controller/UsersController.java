da335accb4ce45f3fa96e6ee9c364b87deb1ac34
guptha <ratnajiguptha819@gmail.com>
2023-06-12 16:06:09 +0530

    change password api created.

--- ekart-backend/Jwt-security/src/main/java/com/ekart/jwt/controller/UsersController.java
+++ ekart-backend/Jwt-security/src/main/java/com/ekart/jwt/controller/UsersController.java
@@ -3,7 +3,9 @@ package com.ekart.jwt.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +19,9 @@ public class UsersController {
 
 	@Autowired
 	private CustomerRepo customerRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
 
 	@GetMapping("/getUserName/{userName}")
 	public ResponseEntity<?> getUserByUserName(@PathVariable String userName) {
@@ -65,6 +70,19 @@ public class UsersController {
 			System.out.println(user.get().getContactNumber());
 			return ResponseEntity.ok(user.get().getContactNumber());
 		}
+	}
+	
+	@PutMapping("/updatePasswordByUserName/{userName}/{password}")
+	public ResponseEntity<?> updatePassword(@PathVariable String userName, @PathVariable String password){
+		
+		CustomerEntity customer = customerRepo.findByUserName(userName).get();
+		
+		customer.setPassword(encoder.encode(password));
+		
+		customerRepo.save(customer);
+		
+		return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("updated password");
+		
 	}
 	
 }
