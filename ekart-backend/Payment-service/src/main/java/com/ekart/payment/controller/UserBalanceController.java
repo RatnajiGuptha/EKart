4a779be5cdc960c44027bbc9b4b23430ad6e85d5
guptha <ratnajiguptha819@gmail.com>
2023-06-21 20:00:56 +0530

    add user balance added

--- ekart-backend/Payment-service/src/main/java/com/ekart/payment/controller/UserBalanceController.java
+++ ekart-backend/Payment-service/src/main/java/com/ekart/payment/controller/UserBalanceController.java
@@ -11,39 +11,36 @@ import java.util.List;
 @RequestMapping("/api/userBalance")
 public class UserBalanceController {
 
-    @Autowired
-    private UserBalanceRepository userBalanceRepository;
+	@Autowired
+	private UserBalanceRepository userBalanceRepository;
 
-    @PostMapping("/add")
-    public String saveUserBalance(@RequestBody UserBalance userBalance){
-        userBalanceRepository.save(userBalance);
-        return "Balnce Saved";
-    }
+	@PostMapping("/add")
+	public String saveUserBalance(@RequestBody UserBalance userBalance) {
+		userBalanceRepository.save(userBalance);
+		return "Balnce Saved";
+	}
 
-    @GetMapping("/getUserBalance")
-    public List<UserBalance> getUserBalance(){
-        return userBalanceRepository.findAll();
-    }
+	@GetMapping("/getBalanceByEmail/{email}")
+	public int getBalanceByEmail(@PathVariable String email) {
+		UserBalance userBalance = userBalanceRepository.findByEmail(email);
 
-    
-    
-    @GetMapping("/getBalanceByEmail/{email}")
-    public int getBalanceByEmail(@PathVariable String email){
-        UserBalance userBalance = userBalanceRepository.findByEmail(email);
+		return userBalance.getPrice();
+	}
 
-        return userBalance.getPrice();
-    }
-    
-    @PutMapping("/updateBalanceByEmail/{email}/{amount}")
-    public  String updateUserBalanceByEmail(@PathVariable String email,@PathVariable int amount){
-        UserBalance ub = userBalanceRepository.findByEmail(email);
-        ub.setPrice(ub.getPrice() + amount);
-        userBalanceRepository.save(ub);
-        return "user Balance Updated";
-    }
-    
-    
-    
-    
-   
+	@PutMapping("/updateBalanceByEmail/{email}/{amount}")
+	public String updateUserBalanceByEmail(@PathVariable String email, @PathVariable int amount) {
+		UserBalance ub = userBalanceRepository.findByEmail(email);
+		ub.setPrice(ub.getPrice() + amount);
+		userBalanceRepository.save(ub);
+		return "user Balance Updated";
+	}
+
+	@PostMapping("/addBalanceByEmail/{email}/{amount}")
+	public String addUserBalanceByEmail(@PathVariable String email, @PathVariable int amount) {
+		UserBalance ub = userBalanceRepository.findByEmail(email);
+		ub.setPrice(amount);
+		userBalanceRepository.save(ub);
+		return "user Balance Updated";
+	}
+
 }
